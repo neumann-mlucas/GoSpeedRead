adf57502af8e7e486e7ea8bcdfb65af8f0866ce2
neumann-mlucas <neumann.m.lucas@gmail.com>
2024-02-02 23:28:51 -0300

    add get next and previus words methods

--- internal/displaytext/displaytext.go
+++ internal/displaytext/displaytext.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"github.com/atotto/clipboard"
 	"internal/words"
+	"strings"
 	"time"
 )
 
 const (
-	LOREN = `
+	LOREM = `
 Lorem ipsum dolor sit amet, "consectetur adipiscing elit. Nunc rutrum tincidunt massa". Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum vel elit ut nisi fermentum lacinia et et dolor. Aliquam lacinia et lacus eget ultrices. Phasellus at arcu ex. Donec interdum venenatis mi eu tempus. Proin convallis est libero, ut luctus dolor lobortis eu. Etiam a egestas odio. Donec vitae nisl mauris. Praesent interdum lectus quis odio maximus, a posuere turpis rutrum.
 Donec quis libero ipsum. Nulla faucibus sapien pulvinar aliquet pretium. Aliquam quis mattis nulla. Donec sodales viverra convallis. Integer mollis luctus orci id vulputate. In eget sem lobortis, congue arcu ac, scelerisque velit. Quisque nec consequat massa. Etiam mattis accumsan porta. Nulla rutrum sapien felis, egestas fermentum elit molestie ut. Suspendisse viverra convallis risus, non rutrum metus lacinia sed.
 Etiam a malesuada odio, et luctus nunc. Cras sit amet scelerisque justo, a tincidunt nibh. Sed aliquam pretium imperdiet. Etiam quis egestas lectus, eu volutpat dui. Vivamus congue tincidunt nulla, sit amet vulputate risus mattis ac. Ut dapibus odio nisi, eu posuere augue luctus eget. Donec eu arcu eu ante ultricies sollicitudin. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec nec arcu ut eros rhoncus vulputate nec eu magna. Cras hendrerit gravida libero vel condimentum. Vivamus augue massa, volutpat non interdum sit amet, ultricies nec turpis. Phasellus eget finibus enim, a tempus ligula. Maecenas metus velit, tincidunt in libero sed, consequat consectetur urna. Proin a interdum quam. Integer ultrices viverra tristique.
@@ -26,6 +27,8 @@ type DisplayState struct {
 	Text string
 	Time time.Duration
 	Prct float64
+	Next string
+	Prev string
 }
 
 // NewDisplayText Constructs a DisplayText object
@@ -44,8 +47,6 @@ func (t *DisplayText) GetClipBoard() {
 		fmt.Println("Error reading clipboard:", err)
 		content = "ERROR_WHILE_READING_CLIPBOARD"
 	}
-	content = LOREN
-
 	words := words.ParseWords(content)
 	t.Words = words
 }
@@ -54,7 +55,13 @@ func (t *DisplayText) GetClipBoard() {
 func (t *DisplayText) Step() DisplayState {
 	defer t.IncIndex(+1)
 	word := t.Words[t.Index]
-	return DisplayState{word.String(), t.DisplayTime(word), t.Percentage()}
+	return DisplayState{
+		Text: word.String(),
+		Time: t.DisplayTime(word),
+		Prct: t.Percentage(),
+		Next: t.GetNextWords(5),
+		Prev: t.GetPreviusWords(5),
+	}
 }
 
 // DisplayTime calculates the display time in millisecond of a given Word
@@ -75,7 +82,12 @@ func (t *DisplayText) IncIndex(inc int) {
 
 // IsLastElement checks if the index is at the last element of a collection.
 func (t *DisplayText) IsLastWord() bool {
-	return t.Index >= len(t.Words)-1
+	return t.Index >= len(t.Words)
+}
+
+// IsFirstOrLastWord returns true if Index it at the first or last position
+func (t *DisplayText) IsFirstOrLastWord() bool {
+	return t.Index == 0 || t.Index >= len(t.Words)-1
 }
 
 // Percentage returns the relative position of the index
@@ -83,7 +95,28 @@ func (t *DisplayText) Percentage() float64 {
 	return float64(t.Index) / float64(len(t.Words))
 }
 
-// AtStartOrEnd returns true if Index it at the first or last position
-func (t *DisplayText) AtStartOrEnd() bool {
-	return t.Index == 0 || t.Index == len(t.Words)
+// GetPreviusWords returns a string with N words come before the current word
+func (t *DisplayText) GetPreviusWords(i int) string {
+	if t.Index <= 0 {
+		return ""
+	}
+	var words []string
+	for _, w := range t.Words[max(0, t.Index-i):t.Index] {
+		words = append(words, w.Text)
+
+	}
+	return strings.Join(words, " ")
+}
+
+// GetNextWords returns a string with N words come after the current word
+func (t *DisplayText) GetNextWords(i int) string {
+	if t.IsLastWord() {
+		return ""
+	}
+	var words []string
+	for _, w := range t.Words[t.Index+1 : min(len(t.Words), t.Index+i+1)] {
+		words = append(words, w.Text)
+
+	}
+	return strings.Join(words, " ")
 }
